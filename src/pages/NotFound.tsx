7e59d99b3f1c41893b4d6d559f1d7ff980a9fd02
Augusto <ampiatto@hotmail.com>
2023-08-24 16:12:44 -0300

    ajuste da tela de nao encontrado

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -2,7 +2,7 @@ import styles from "./NotFound.module.css";
 
 const NotFound = () => {
   return (
-    <div className={`${styles.container} ${styles.mainContainer}`}>
+    <div className={styles.container}>
       <h1 className={styles.title}>Erro: 404</h1>
       <p>Página não encontrada</p>
     </div>
